5938ab63d9f124c6b0f19dcf5fb9fec67f8e3ac6
Alvin <xrsenelupin6197@gmail.com>
2024-02-18 22:15:58 +0800

    pc

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("嗨");
+
+
+// 字串string "hi"
+// 字元char 'X'
+// 整數 int 67
+// 浮點數 double 67.2
+// 布林值 bool true false
